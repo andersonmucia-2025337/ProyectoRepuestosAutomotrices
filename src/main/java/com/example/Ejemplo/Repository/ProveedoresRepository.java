5f127849c9c16ecb2f3deec5a26ebd1188891fde
andersonmucia-2025337 <andersonmucia-2025337@kinal.edu.gt>
2026-02-13 20:43:19 -0600

    feat: clase repository por parte de la entidad Proveedores operando con CRUD

--- src/main/java/com/example/Ejemplo/Repository/ProveedoresRepository.java
+++ src/main/java/com/example/Ejemplo/Repository/ProveedoresRepository.java
@@ -1,4 +1,10 @@
 package com.example.Ejemplo.Repository;
 
-public interface ProveedoresRepository {
+import org.springframework.stereotype.Repository;
+import com.example.Ejemplo.Entity.Proveedores;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface ProveedoresRepository extends JpaRepository<Proveedores, Integer>{
+
 }
