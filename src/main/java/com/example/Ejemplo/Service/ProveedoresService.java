0037da70ff3b3160525d6df8196b5a9190ae8731
andersonmucia-2025337 <andersonmucia-2025337@kinal.edu.gt>
2026-02-13 21:19:55 -0600

    feat: clase service por parte de la entidad Proveedores operando con CRUD

--- src/main/java/com/example/Ejemplo/Service/ProveedoresService.java
+++ src/main/java/com/example/Ejemplo/Service/ProveedoresService.java
@@ -1,4 +1,15 @@
 package com.example.Ejemplo.Service;
 
-public interface ProveedorService {
+import com.example.Ejemplo.Entity.Proveedores;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ProveedoresService {
+    List<Proveedores> getAllProveedores();
+    Proveedores getIdProveedores(Integer id) throws RuntimeException;
+    Proveedores saveProveedores(Proveedores proveedores) throws RuntimeException;
+    Proveedores updateProveedor(Integer id, Proveedores proveedor) throws RuntimeException;
+    void deleteProveedor(Integer id) throws RuntimeException;
 }
