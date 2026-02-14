7e952d4f03826954a5975b2bf3df31f7e86ea5fa
andersonmucia-2025337 <andersonmucia-2025337@kinal.edu.gt>
2026-02-13 21:21:05 -0600

    feat: clase service por parte de la entidad Repuestos operando con CRUD

--- src/main/java/com/example/Ejemplo/Service/RepuestosService.java
+++ src/main/java/com/example/Ejemplo/Service/RepuestosService.java
@@ -1,4 +1,14 @@
 package com.example.Ejemplo.Service;
 
+import com.example.Ejemplo.Entity.Repuestos;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public interface RepuestosService {
+    List<Repuestos> getAllRepuestos();
+    Repuestos getRepuestoById(Integer id);
+    Repuestos saveRepuesto(Repuestos repuesto) throws RuntimeException;
+    Repuestos updateRepuesto(Integer id, Repuestos repuesto) throws RuntimeException;
+    void deleteRepuesto(Integer id) throws RuntimeException;
 }
