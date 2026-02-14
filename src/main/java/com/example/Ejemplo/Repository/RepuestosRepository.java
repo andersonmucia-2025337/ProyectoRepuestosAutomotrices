0eb502eb752f8c764d3b4f79e588c5b7d722bf8f
andersonmucia-2025337 <andersonmucia-2025337@kinal.edu.gt>
2026-02-13 20:43:52 -0600

    feat: clase repository por parte de la entidad Repuestos operando con CRUD

--- src/main/java/com/example/Ejemplo/Repository/RepuestosRepository.java
+++ src/main/java/com/example/Ejemplo/Repository/RepuestosRepository.java
@@ -1,4 +1,10 @@
 package com.example.Ejemplo.Repository;
 
-public interface RepuestosRepository {
+import com.example.Ejemplo.Entity.Repuestos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepuestosRepository extends JpaRepository<Repuestos, Integer> {
+
 }
