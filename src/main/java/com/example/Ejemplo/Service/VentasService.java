7da5bb0216ea0930875269a74acb4c622e0db195
andersonmucia-2025337 <andersonmucia-2025337@kinal.edu.gt>
2026-02-13 21:21:59 -0600

    feat: clase service por parte de la entidad Ventas operando con CRUD

--- src/main/java/com/example/Ejemplo/Service/VentasService.java
+++ src/main/java/com/example/Ejemplo/Service/VentasService.java
@@ -1,4 +1,14 @@
 package com.example.Ejemplo.Service;
 
+import com.example.Ejemplo.Entity.Ventas;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public interface VentasService {
+    List<Ventas> getAllVentas();
+    Ventas getVentaById(Integer id);
+    Ventas saveVenta(Ventas venta) throws RuntimeException;
+    Ventas updateVenta(Integer id, Ventas venta) throws RuntimeException;
+    void deleteVenta(Integer id) throws RuntimeException;
 }
