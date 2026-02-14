db892b68db82cb87148ecfd71bddf65f9da96625
andersonmucia-2025337 <andersonmucia-2025337@kinal.edu.gt>
2026-02-13 20:44:14 -0600

    feat: clase repository por parte de la entidad Ventas operando con CRUD

--- src/main/java/com/example/Ejemplo/Repository/VentasRepository.java
+++ src/main/java/com/example/Ejemplo/Repository/VentasRepository.java
@@ -1,4 +1,10 @@
 package com.example.Ejemplo.Repository;
 
-public class VentasRepository {
+import com.example.Ejemplo.Entity.Ventas;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VentasRepository extends JpaRepository<Ventas, Integer> {
+
 }
